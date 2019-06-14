f948b433fb3c0950859eddf40d549c30ff7b3dba
Training_C2a.05.08 <Training_C2a.05.08@CDC2-D-513RJHF.dir.svc.accenture.com>
2019-06-14 17:18:31 +0530

    second commit

--- src/test/java/github_project/eg2.java
+++ src/test/java/github_project/eg2.java
@@ -2,9 +2,14 @@ package github_project;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin={"pretty","json:target/cucumber.json"}
+		
+)
 public class eg2 {
 
 }
--- src/test/java/github_project/eg3.java
+++ src/test/java/github_project/eg3.java
@@ -1,5 +1,16 @@
 package github_project;
 
+import cucumber.api.java.en.Given;
+
 public class eg3 {
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+	   System.out.println("username");
+	}
+
+	@Given("some other precondition")
+	public void some_other_precondition() {
+	  System.out.println("password");
+	}
 
 }
